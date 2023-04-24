45c6e528944a7f5d5c1e2e8a4f1b29cb3bb7d2e8
emeraldswag312 <79111172+emeraldswag312@users.noreply.github.com>
2023-04-24 16:42:15 -0400

    Update routes.py

--- flask_app/users/routes.py
+++ flask_app/users/routes.py
@@ -11,7 +11,7 @@ users=Blueprint('users', __name__)
 @users.route("/register", methods=["GET", "POST"])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for("movies.index"))
+        return redirect(url_for("games.index"))
 
     form = RegistrationForm()
     if form.validate_on_submit():
@@ -27,7 +27,7 @@ def register():
 @users.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for("movies.index"))
+        return redirect(url_for("games.index"))
 
     form = LoginForm()
     if form.validate_on_submit():
@@ -49,7 +49,7 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for("movies.index"))
+    return redirect(url_for("games.index"))
 
 
 @users.route("/account", methods=["GET", "POST"])
