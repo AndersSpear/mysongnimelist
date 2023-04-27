40e929020bbf15f3c567c0789b3d2d49f629b86f
Lily Larson <36712046+silly-lily@users.noreply.github.com>
2023-04-27 17:29:00 -0400

    Update utils.py

--- flask_app/utils.py
+++ flask_app/utils.py
@@ -1,5 +1,15 @@
 from datetime import datetime
 
+def calc_avg_rating(reviews):
+    
+    avg = 0
+    
+    for review in reviews:
+        avg+=review.rating
+        
+    avg/=len(review)
+    
+    return avg
 
 def current_time() -> str:
     return datetime.now().getMonth()+'/'+datetime.now().getDay()+'/'+datetime.now().getYear()
