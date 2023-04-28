4efbe814826ecc8020921870ffb76987a4fc64d0
aspear <anders@aspear.net>
2023-04-27 20:37:38 -0400

    CLIENT FUCKOING WORKS

--- flask_app/client.py
+++ flask_app/client.py
@@ -1,79 +1,32 @@
-import requests
-from . import config
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 
-class Game(object):
-    def __init__(self, omdb_json, detailed=False):
+class Song(object):
+    def __init__(self, songdict, detailed=False):
         if detailed:
-            self.ratings_count = omdb_json["Ratings"]
-            self.playtime = omdb_json["Playtime"]
-            self.esrb_rating = omdb_json["ESRB_Rating"]
-
-        self.title = omdb_json["Title"]
-        self.rating = omdb_json["Rating"]
-        self.rawg_ID = omdb_json["rawgID"]
-        self.type = "Game"
-        self.background_url = omdb_json["Background_Image"]
+            self.song_popularity = songdict["popularity"]
+            self.song_preview_url = songdict["url"]
+            self.song_is_explicit = songdict["is_explicit"]
+            for artist in songdict["artists"]:
+                self.song_full_artist_list.append(artist["name"])
+            self.album_image = songdict["album"]["images"][0]["url"]
+        self.song_name = songdict["name"]
+        self.song_primary_artist = songdict["artists"][0]["name"]
+        self.album_name = songdict["album"]["name"]
+        self.type = "Song"
+        self.song_image = songdict["image"]
+        self.song_id = songdict["id"]
 
     def __repr__(self):
-        return self.title
+        return ("Name: " + self.song_name + "\nArtist: " + self.song_primary_artist + "\nAlbum: " + self.album_name+"\nSong ID: "+self.song_id+"\n")
 
 
-class GameClient(object):
-    def __init__(self, api_key):
-        self.sess = requests.Session()
-        self.base_url = f"https://api.rawg.io/api/games?key={api_key}"
-
-    def search(self, search_string):
-        """
-        Searches the API for the supplied search_string, and returns
-        a list of Media objects if the search was successful, or the error response
-        if the search failed.
-
-        Only use this method if the user is using the search bar on the website.
-        """
-        search_string = "+".join(search_string.split())
-        page = 1
-
-        search_url = f"s={search_string}&page={page}"
-
-        resp = self.sess.get(self.base_url + search_url)
-
-        if resp.status_code != 200:
-            raise ValueError(
-                "Search request failed; make sure your API key is correct and authorized"
-            )
-
-        data = resp.json()
-
-        if data["Response"] == "False":
-            raise ValueError(
-                f'[ERROR]: Error retrieving results: \'{data["Error"]}\' ')
-
-        search_results_json = data["Search"]
-        remaining_results = int(data["totalResults"])
-
-        result = []
-
-        # We may have more results than are first displayed
-        while remaining_results != 0:
-            for item_json in search_results_json:
-                result.append(Movie(item_json))
-                remaining_results -= len(search_results_json)
-            page += 1
-            search_url = f"s={search_string}&page={page}"
-            resp = self.sess.get(self.base_url + search_url)
-            if resp.status_code != 200 or resp.json()["Response"] == "False":
-                break
-            search_results_json = resp.json()["Search"]
-
-        return result
-
-
-class MovieClient(object):
-    def __init__(self, api_key):
-        self.sess = requests.Session()
-        self.base_url = f"http://www.omdbapi.com/?apikey={api_key}&r=json&type=movie&"
+class SongClient(object):
+    def __init__(self):
+        creds = SpotifyClientCredentials(
+            client_id=os.environ.get('SPOTIPY_CLIENT_ID'), client_secret=os.environ.get('SPOTIPY_CLIENT_SECRET'))
+        self.sp = spotipy.Spotify(auth_manager=creds)
 
     def search(self, search_string):
         """
@@ -83,76 +36,56 @@ class MovieClient(object):
 
         Only use this method if the user is using the search bar on the website.
         """
-        search_string = "+".join(search_string.split())
-        page = 1
+        result = self.sp.search(search_string, limit=50, offset=0,
+                                type='track', market=None)
 
-        search_url = f"s={search_string}&page={page}"
+        songlist = []
+        try:
+            for track in result["tracks"]["items"]:
+                songdict = {}
+                songdict["name"] = track["name"]
+                songdict["artists"] = track["artists"]
+                songdict["album"] = track["album"]
+                songdict["image"] = track["album"]["images"][0]["url"]
+                songdict["id"] = track["id"]
+                songdict["popularity"] = track["popularity"]
+                songdict["is_explicit"] = track["explicit"]
+                songdict["url"] = track["preview_url"]
+                songlist.append(songdict)
+            return songlist
+        except:
+            print("Error :(")
+            return None
 
-        resp = self.sess.get(self.base_url + search_url)
-
-        if resp.status_code != 200:
-            raise ValueError(
-                "Search request failed; make sure your API key is correct and authorized"
-            )
-
-        data = resp.json()
-
-        if data["Response"] == "False":
-            raise ValueError(
-                f'[ERROR]: Error retrieving results: \'{data["Error"]}\' ')
-
-        search_results_json = data["Search"]
-        remaining_results = int(data["totalResults"])
-
-        result = []
-
-        # We may have more results than are first displayed
-        while remaining_results != 0:
-            for item_json in search_results_json:
-                result.append(Movie(item_json))
-                remaining_results -= len(search_results_json)
-            page += 1
-            search_url = f"s={search_string}&page={page}"
-            resp = self.sess.get(self.base_url + search_url)
-            if resp.status_code != 200 or resp.json()["Response"] == "False":
-                break
-            search_results_json = resp.json()["Search"]
-
-        return result
-
-    def retrieve_movie_by_id(self, imdb_id):
+    def get_song(self, song_id):
         """
-        Use to obtain a Movie object representing the movie identified by
-        the supplied imdb_id
+        Searches the API for the supplied search_string, and returns
+        a list of Media objects if the search was successful, or the error response
+        if the search failed.
+
+        Only use this method if the user is using the search bar on the website.
         """
-        movie_url = self.base_url + f"i={imdb_id}&plot=full"
+        result = self.sp.track(song_id)
 
-        resp = self.sess.get(movie_url)
-
-        if resp.status_code != 200:
-            raise ValueError(
-                "Search request failed; make sure your API key is correct and authorized"
-            )
-
-        data = resp.json()
-
-        if data["Response"] == "False":
-            raise ValueError(f'Error retrieving results: \'{data["Error"]}\' ')
-
-        movie = Movie(data, detailed=True)
-
-        return movie
+        songdict = {}
+        songdict["name"] = result["name"]
+        songdict["artists"] = result["artists"]
+        songdict["album"] = result["album"]
+        songdict["image"] = result["album"]["images"][0]["url"]
+        songdict["id"] = result["id"]
+        songdict["popularity"] = result["popularity"]
+        songdict["is_explicit"] = result["explicit"]
+        songdict["url"] = result["preview_url"]
+        return songdict
 
 
 ## -- Example usage -- ###
 if __name__ == "__main__":
     import os
 
-    client = GameClient(API_KEY)
+    client = SongClient()
+    # print(Song(client.search("star")[0]))
+    print(Song(client.get_song("7MXVkk9YMctZqd1Srtv4MB")))
 
-    movies = client.search("guardians")
 
-    for movie in movies:
-        print(movie)
-
-    print(len(movies))
+# {'disc_number': 1, 'duration_ms': 267066, 'explicit': False, 'external_ids': {'isrc': 'USCM51500238'}, 'external_urls': {'spotify': 'https://open.spotify.com/track/0wwPcA6wtMf6HUMpIRdeP7'}, 'href': 'https://api.spotify.com/v1/tracks/0wwPcA6wtMf6HUMpIRdeP7', 'id': '0wwPcA6wtMf6HUMpIRdeP7', 'is_local': False, 'name': 'Hotline Bling', 'popularity': 80, 'preview_url': 'https://p.scdn.co/mp3-preview/2541ea8f31c87a3bda268b2b7a25bde73530e07d?cid=43c32308e1f940cba0cbcda5c0ecb6d6', 'track_number': 20, 'type': 'track', 'uri': 'spotify:track:0wwPcA6wtMf6HUMpIRdeP7'}
