db828c249529e41eec4515d12fa66a67afe02188
aspear <anders@aspear.net>
2023-05-01 01:24:22 -0400

    added discord lol

---
+++ flask_app/discord/routes.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, redirect, url_for, render_template, flash, request
+from flask_login import current_user, login_required, login_user, logout_user
+from flask_dance.contrib.discord import make_discord_blueprint, discord
+
+from .. import bcrypt
+from ..forms import RegistrationForm, LoginForm, UpdateUsernameForm
+from ..models import User
+
+discordd = make_discord_blueprint(
+    client_id="1102379414114807878",
+    client_secret="LAgNwjE9Qxh_DOZJWmipPOMFVuLSlEYe"
+)
+
+
+@discordd.route("/log")
+def log():
+    if not discord.authorized:
+        return redirect(url_for("discord.login"))
+    resp = discord.get("/api/users/@me")
+    return redirect(url_for("songs.index"))
+
+# @app.route("/")
+# def index():
+#     if not discord.authorized:
+#         return redirect(url_for("discord.login"))
+#     resp = discord.get("/api/users/@me")
+#     return render_template("song.html")
+
+
+# @app.route("/logout")
+# def logout():
+#     session.clear()
+#     return redirect(url_for('index'))
